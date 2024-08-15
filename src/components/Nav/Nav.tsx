37c93a406f2bbd61358f22282807bafbc0d94d1e
MaksSamohin <user.name>
2024-08-15 16:10:15 +0300

    feat: adaptive design for all pages

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -6,6 +6,10 @@ import {
   MenuItem,
   Input,
   Box,
+  List,
+  Drawer,
+  ListItem,
+  Typography,
 } from "@mui/material";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
@@ -24,6 +28,8 @@ import AnalyticsIcon from "@mui/icons-material/Analytics";
 import SearchIcon from "@mui/icons-material/Search";
 import { usePathname } from "next/navigation";
 import { fetchUserData } from "@/store/userSlice";
+import MenuIcon from "@mui/icons-material/Menu";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 interface NavProps {
   setSearchText: (text: string) => void;
@@ -37,6 +43,7 @@ export default function Nav({ setSearchText }: NavProps) {
   const pathname = usePathname();
   const [loading, setLoading] = useState(true);
   const [isHomePage, setIsHomepage] = useState(false);
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   useAuth(setLoading);
 
@@ -54,6 +61,57 @@ export default function Nav({ setSearchText }: NavProps) {
       dispatch(fetchUserData(user.uid));
     }
   }, [user.uid, route, loading]);
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setDrawerOpen(open);
+  };
+
+  const list = () => (
+    <Box
+      sx={{}}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+      className={styles.drawerBox}
+    >
+      <List className={styles.drawerBoxList}>
+        <ListItem>
+          <Link href="/" className={styles.link}>
+            <DashboardIcon sx={{ fontSize: 45 }} />
+            Dashboard
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link href="/edit" className={styles.link}>
+            <EditNoteIcon sx={{ fontSize: 45 }} />
+            Add/Edit
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link href="/edit" className={styles.link}>
+            <AnalyticsIcon sx={{ fontSize: 45 }} />
+            Analysis
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link className={styles.link} href="/personalcabinet">
+            <AccountCircleIcon sx={{ fontSize: 45 }} />
+            Personal cabinet
+          </Link>
+        </ListItem>
+        <ListItem className={styles.link} onClick={handleLogout}>
+          <LogoutIcon sx={{ fontSize: 45 }} />
+          Log out
+        </ListItem>
+      </List>
+    </Box>
+  );
 
   const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -74,54 +132,71 @@ export default function Nav({ setSearchText }: NavProps) {
     }
   };
   return (
-    <AppBar>
-      <Container className={styles.bar}>
-        <ul className={styles.linkList}>
-          <li>
-            <Link href="/" className={styles.link}>
-              <DashboardIcon sx={{ fontSize: 30 }} />
-              Dashboard
-            </Link>
-          </li>
-          <li>
-            <Link href="/edit" className={styles.link}>
-              <EditNoteIcon sx={{ fontSize: 45 }} />
-              Add/Edit
-            </Link>
-          </li>
-          <li>
-            <Link href="/edit" className={styles.link}>
-              <AnalyticsIcon sx={{ fontSize: 30 }} />
-              Analysis
-            </Link>
-          </li>
-        </ul>
-        {isHomePage && (
-          <Box className={styles.searchField}>
-            <SearchIcon sx={{ fontSize: 35 }} />
-            <Input
-              className={styles.searchInput}
-              onChange={(e) => setSearchText(e.target.value)}
-              placeholder="Find the note"
-            />
-          </Box>
-        )}
+    <>
+      <AppBar>
+        <Container className={styles.bar}>
+          <Button
+            className={styles.mobileMenu}
+            onClick={toggleDrawer(!drawerOpen)}
+          >
+            <MenuIcon sx={{ color: "white", fontSize: 45 }} />
+          </Button>
+          <ul className={styles.linkList}>
+            <li>
+              <Link href="/" className={styles.link}>
+                <DashboardIcon sx={{ fontSize: 30 }} />
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link href="/edit" className={styles.link}>
+                <EditNoteIcon sx={{ fontSize: 45 }} />
+                Add/Edit
+              </Link>
+            </li>
+            <li>
+              <Link href="/edit" className={styles.link}>
+                <AnalyticsIcon sx={{ fontSize: 30 }} />
+                Analysis
+              </Link>
+            </li>
+          </ul>
 
-        <Button onClick={handleMenuOpen} className={styles.emailButton}>
-          <AccountCircleIcon sx={{ fontSize: 40 }} />
-          {user.displayName ? user.displayName : user.email}
-        </Button>
-        <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleMenuClose}
-        >
-          <Link className={styles.personalcabinetLink} href="/personalcabinet">
-            <MenuItem>Personal cabinet</MenuItem>
-          </Link>
-          <MenuItem onClick={handleLogout}>Log out</MenuItem>
-        </Menu>
-      </Container>
-    </AppBar>
+          {isHomePage && (
+            <Box className={styles.searchField}>
+              <SearchIcon className={styles.searchIcon} sx={{ fontSize: 45 }} />
+              <Input
+                className={styles.searchInput}
+                onChange={(e) => setSearchText(e.target.value)}
+                placeholder="Find the note"
+              />
+            </Box>
+          )}
+
+          <Button onClick={handleMenuOpen} className={styles.emailButton}>
+            <AccountCircleIcon sx={{ fontSize: 40 }} />
+            <Typography className={styles.emailDisplayText}>
+              {user.displayName ? user.displayName : user.email}
+            </Typography>
+          </Button>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <Link
+              className={styles.personalcabinetLink}
+              href="/personalcabinet"
+            >
+              <MenuItem>Personal cabinet</MenuItem>
+            </Link>
+            <MenuItem onClick={handleLogout}>Log out</MenuItem>
+          </Menu>
+        </Container>
+      </AppBar>
+      <Drawer anchor="top" open={drawerOpen} onClose={toggleDrawer(false)}>
+        {list()}
+      </Drawer>
+    </>
   );
 }
